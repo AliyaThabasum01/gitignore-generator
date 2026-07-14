2c784572d5fbffb2725dba3b22037151d09bd7f8
Aliya Thabasum <coolaliya07@gmail.com>
2026-07-14 17:01:00 +0530

    Add template for ignoring files in Python, Node, and Java

---
+++ templates.py
@@ -0,0 +1,21 @@
+templates = {
+    "python": """__pycache__/
+*.pyc
+.env
+.vscode/
+.idea/
+""",
+
+    "node": """node_modules/
+.env
+dist/
+coverage/
+""",
+
+    "java": """.classpath
+.project
+.settings/
+target/
+*.class
+"""
+}
